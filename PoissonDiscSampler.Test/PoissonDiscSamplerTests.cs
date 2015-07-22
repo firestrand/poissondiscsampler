e378a99d2719f4d76e484c00cea6b270edcfcf9d
Travis Silvers <firestrand@gmail.com>
2015-07-22 09:41:54 -0400

    Created Solution Bound Length Test

---
+++ PoissonDiscSampler.Test/PoissonDiscSamplerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PoissonDiscSampler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PoissonDiscSamplerTests
+{
+    [TestClass]
+    public class PoissonDiscSamplerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PoissonDiscSamplerBoundLengthDifferenceThrowsException()
+        {
+            var min = new[] {0.0, 0.0};
+            var max = new[] {10.0};
+            var sampler = new PoissonDiscSampler.PoissonDiscSampler(5.0, 30, min, max);
+        }
+    }
+}
---
+++ PoissonDiscSampler/PoissonDiscSampler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoissonDiscSampler
+{
+    public class PoissonDiscSampler
+    {
+
+        public PoissonDiscSampler(double radius, int maxSamples, double[] xMin, double[] xMax)
+        {
+            if (xMin.Length != xMax.Length) throw new ArgumentOutOfRangeException("xMin","min and max bounds are invalid");
+        }
+    }
+}
